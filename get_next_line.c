6b63e5603c8e95465fb13105c3a81028e6abfd6e
Louis Girerd <lgirerd@z4r4p2.42lyon.fr>
2024-11-18 18:04:52 +0100

    going home

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,24 @@
 /*   By: lgirerd <lgirerd@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 16:06:16 by lgirerd           #+#    #+#             */
-/*   Updated: 2024/11/18 16:07:09 by lgirerd          ###   ########lyon.fr   */
+/*   Updated: 2024/11/18 16:49:00 by lgirerd          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 1024
+#endif
+
 #include "get_next_line.h"
 
-char	*get_next_line(int ds)
+char	*get_next_line(int fd)
 {
-	return (NULL);
+	char	*buffer;
+	int		bytesread;
+
+	if (fd == -1)
+		return (NULL);
+	buffer = malloc(BUFFER_SIZE * sizeof(char));
+	bytesread = read(fd, buffer, BUFFER_SIZE);
+	return(buffer);
 }
--- testing.c
+++ testing.c
@@ -1,17 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   testing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lgirerd <lgirerd@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/18 16:00:30 by lgirerd           #+#    #+#             */
-/*   Updated: 2024/11/18 17:31:56 by lgirerd          ###   ########lyon.fr   */
+/*   Created: 2024/11/18 17:29:48 by lgirerd           #+#    #+#             */
+/*   Updated: 2024/11/18 17:55:49 by lgirerd          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include "get_next_line.h"
+#include <stdio.h>
 
-char	*get_next_line(int fd);
+int	main(int ac, char **av)
+{
+	(void)ac;
+	int fd = open(av[1], O_NONBLOCK | O_RDONLY);
+	char *returned = get_next_line(fd);
+	printf("%s", returned);
+	free(returned);
+	return (0);
+}
