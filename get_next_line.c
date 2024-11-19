e235b6c9f1c8849f852077b6cdae0bd4be4ee827
Louis Girerd <lgirerd@z4r9p6.42lyon.fr>
2024-11-19 15:14:37 +0100

    wip

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,12 @@
 /*   By: lgirerd <lgirerd@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 16:06:16 by lgirerd           #+#    #+#             */
-/*   Updated: 2024/11/19 14:53:55 by lgirerd          ###   ########lyon.fr   */
+/*   Updated: 2024/11/19 15:13:23 by lgirerd          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUFFER_SIZE
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 10
 #endif
 
 #include "get_next_line.h"
@@ -21,18 +21,34 @@
 
 char	*get_next_line(int fd)
 {
-	char	*buffer;
-	int		bytesread;
+	static t_list	*stack;
+	char			*line;
+	int				bytesread;
 
-	if (fd == -1)
+	stack = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &line, 0) < 0) // avance pas la tete de lecture mais check la lecture du fd
 		return (NULL);
-	buffer = malloc(BUFFER_SIZE * sizeof(char));
-	bytesread = read(fd, buffer, BUFFER_SIZE);
-	buffer[bytesread - 1] = '\0';
-	return(buffer);
+	bytesread = 1;
+	line = NULL;
+	// on lit
+	read_to_stack(fd, &stack, &bytesread); 
+	// on met line dans la stack
+	// on clear et on free la stack
+	return (line);
 }
 
-void	ft_lstinit(t_list **lst, int fd)
+void	read_to_stack(int fd, t_list **stack, int *bytesread_ptr)
 {
+	char	*buf;
 
+	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (buf == NULL)
+		return ;
+	while (!newline(*stack) && *bytesread_ptr != 0)
+	{
+	*bytesread_ptr = (int)read(fd, buf, BUFFER_SIZE);
+	buf[*bytesread_ptr] = '\0';
+	}
 }
+
+int	newline()
